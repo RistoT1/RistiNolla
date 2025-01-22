cc511800b620f1d863a9ad4f7378603e2f31e334
Risto <ristotoiv.rt@gmail.com>
2025-01-22 19:04:40 +0200

    RistiNolla

---
+++ Ristinolla V1.0/Ristinolla V1.0/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Ristinolla_V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b0_0 = new System.Windows.Forms.Button();
+            this.b0_1 = new System.Windows.Forms.Button();
+            this.b1_1 = new System.Windows.Forms.Button();
+            this.b1_0 = new System.Windows.Forms.Button();
+            this.b2_1 = new System.Windows.Forms.Button();
+            this.b2_0 = new System.Windows.Forms.Button();
+            this.b2_2 = new System.Windows.Forms.Button();
+            this.b1_2 = new System.Windows.Forms.Button();
+            this.b0_2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelO = new System.Windows.Forms.Label();
+            this.txtXpisteet = new System.Windows.Forms.TextBox();
+            this.txtOpisteet = new System.Windows.Forms.TextBox();
+            this.VuoroL = new System.Windows.Forms.Label();
+            this.btnNollaa = new System.Windows.Forms.Button();
+            this.btnVaihto = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox_bot = new System.Windows.Forms.CheckBox();
+            this.checkBox_botStart = new System.Windows.Forms.CheckBox();
+            this.IlmoitusL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b0_0
+            // 
+            this.b0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b0_0.Location = new System.Drawing.Point(38, 32);
+            this.b0_0.Name = "b0_0";
+            this.b0_0.Size = new System.Drawing.Size(87, 79);
+            this.b0_0.TabIndex = 0;
+            this.b0_0.UseVisualStyleBackColor = true;
+            // 
+            // b0_1
+            // 
+            this.b0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0_1.Location = new System.Drawing.Point(131, 32);
+            this.b0_1.Name = "b0_1";
+            this.b0_1.Size = new System.Drawing.Size(87, 79);
+            this.b0_1.TabIndex = 1;
+            this.b0_1.UseVisualStyleBackColor = true;
+            // 
+            // b1_1
+            // 
+            this.b1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1_1.Location = new System.Drawing.Point(131, 117);
+            this.b1_1.Name = "b1_1";
+            this.b1_1.Size = new System.Drawing.Size(87, 79);
+            this.b1_1.TabIndex = 3;
+            this.b1_1.UseVisualStyleBackColor = true;
+            // 
+            // b1_0
+            // 
+            this.b1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b1_0.Location = new System.Drawing.Point(38, 117);
+            this.b1_0.Name = "b1_0";
+            this.b1_0.Size = new System.Drawing.Size(87, 79);
+            this.b1_0.TabIndex = 2;
+            this.b1_0.UseVisualStyleBackColor = true;
+            // 
+            // b2_1
+            // 
+            this.b2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2_1.Location = new System.Drawing.Point(131, 202);
+            this.b2_1.Name = "b2_1";
+            this.b2_1.Size = new System.Drawing.Size(87, 79);
+            this.b2_1.TabIndex = 5;
+            this.b2_1.UseVisualStyleBackColor = true;
+            // 
+            // b2_0
+            // 
+            this.b2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2_0.Location = new System.Drawing.Point(38, 202);
+            this.b2_0.Name = "b2_0";
+            this.b2_0.Size = new System.Drawing.Size(87, 79);
+            this.b2_0.TabIndex = 4;
+            this.b2_0.UseVisualStyleBackColor = true;
+            // 
+            // b2_2
+            // 
+            this.b2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2_2.Location = new System.Drawing.Point(224, 202);
+            this.b2_2.Name = "b2_2";
+            this.b2_2.Size = new System.Drawing.Size(87, 79);
+            this.b2_2.TabIndex = 8;
+            this.b2_2.UseVisualStyleBackColor = true;
+            // 
+            // b1_2
+            // 
+            this.b1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1_2.Location = new System.Drawing.Point(224, 117);
+            this.b1_2.Name = "b1_2";
+            this.b1_2.Size = new System.Drawing.Size(87, 79);
+            this.b1_2.TabIndex = 7;
+            this.b1_2.UseVisualStyleBackColor = true;
+            // 
+            // b0_2
+            // 
+            this.b0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0_2.Location = new System.Drawing.Point(224, 32);
+            this.b0_2.Name = "b0_2";
+            this.b0_2.Size = new System.Drawing.Size(87, 79);
+            this.b0_2.TabIndex = 6;
+            this.b0_2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(396, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Pisteet/Score";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelX.Location = new System.Drawing.Point(408, 62);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(26, 25);
+            this.labelX.TabIndex = 10;
+            this.labelX.Text = "X";
+            // 
+            // labelO
+            // 
+            this.labelO.AutoSize = true;
+            this.labelO.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelO.Location = new System.Drawing.Point(457, 62);
+            this.labelO.Name = "labelO";
+            this.labelO.Size = new System.Drawing.Size(28, 25);
+            this.labelO.TabIndex = 11;
+            this.labelO.Text = "O";
+            // 
+            // txtXpisteet
+            // 
+            this.txtXpisteet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXpisteet.Location = new System.Drawing.Point(402, 90);
+            this.txtXpisteet.Multiline = true;
+            this.txtXpisteet.Name = "txtXpisteet";
+            this.txtXpisteet.ReadOnly = true;
+            this.txtXpisteet.Size = new System.Drawing.Size(41, 35);
+            this.txtXpisteet.TabIndex = 12;
+            this.txtXpisteet.Text = "0";
+            this.txtXpisteet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtOpisteet
+            // 
+            this.txtOpisteet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOpisteet.Location = new System.Drawing.Point(450, 90);
+            this.txtOpisteet.Multiline = true;
+            this.txtOpisteet.Name = "txtOpisteet";
+            this.txtOpisteet.ReadOnly = true;
+            this.txtOpisteet.Size = new System.Drawing.Size(41, 35);
+            this.txtOpisteet.TabIndex = 13;
+            this.txtOpisteet.Text = "0";
+            this.txtOpisteet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // VuoroL
+            // 
+            this.VuoroL.AutoSize = true;
+            this.VuoroL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.VuoroL.Location = new System.Drawing.Point(410, 233);
+            this.VuoroL.Name = "VuoroL";
+            this.VuoroL.Size = new System.Drawing.Size(63, 17);
+            this.VuoroL.TabIndex = 14;
+            this.VuoroL.Text = "Vuoro: X";
+            // 
+            // btnNollaa
+            // 
+            this.btnNollaa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnNollaa.Location = new System.Drawing.Point(405, 253);
+            this.btnNollaa.Name = "btnNollaa";
+            this.btnNollaa.Size = new System.Drawing.Size(80, 39);
+            this.btnNollaa.TabIndex = 15;
+            this.btnNollaa.Tag = "Nollaus";
+            this.btnNollaa.Text = "Nollaa";
+            this.btnNollaa.UseVisualStyleBackColor = true;
+            this.btnNollaa.Click += new System.EventHandler(this.btnNollaa_Click);
+            // 
+            // btnVaihto
+            // 
+            this.btnVaihto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnVaihto.Location = new System.Drawing.Point(491, 253);
+            this.btnVaihto.Name = "btnVaihto";
+            this.btnVaihto.Size = new System.Drawing.Size(49, 39);
+            this.btnVaihto.TabIndex = 16;
+            this.btnVaihto.Tag = "Vaihto";
+            this.btnVaihto.Text = "X";
+            this.btnVaihto.UseVisualStyleBackColor = true;
+            this.btnVaihto.Click += new System.EventHandler(this.btnVaihto_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(492, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Vaihda";
+            // 
+            // checkBox_bot
+            // 
+            this.checkBox_bot.AutoSize = true;
+            this.checkBox_bot.Location = new System.Drawing.Point(405, 298);
+            this.checkBox_bot.Name = "checkBox_bot";
+            this.checkBox_bot.Size = new System.Drawing.Size(132, 17);
+            this.checkBox_bot.TabIndex = 18;
+            this.checkBox_bot.Text = "Pelaa robottia vastaan";
+            this.checkBox_bot.UseVisualStyleBackColor = true;
+            this.checkBox_bot.CheckedChanged += new System.EventHandler(this.checkBox_bot_CheckedChanged);
+            // 
+            // checkBox_botStart
+            // 
+            this.checkBox_botStart.AutoSize = true;
+            this.checkBox_botStart.Location = new System.Drawing.Point(307, 298);
+            this.checkBox_botStart.Name = "checkBox_botStart";
+            this.checkBox_botStart.Size = new System.Drawing.Size(92, 17);
+            this.checkBox_botStart.TabIndex = 19;
+            this.checkBox_botStart.Text = "robotti aloittaa";
+            this.checkBox_botStart.UseVisualStyleBackColor = true;
+            this.checkBox_botStart.CheckedChanged += new System.EventHandler(this.checkBox_botStart_CheckedChanged);
+            // 
+            // IlmoitusL
+            // 
+            this.IlmoitusL.AutoSize = true;
+            this.IlmoitusL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.IlmoitusL.ForeColor = System.Drawing.Color.Red;
+            this.IlmoitusL.Location = new System.Drawing.Point(12, 294);
+            this.IlmoitusL.Name = "IlmoitusL";
+            this.IlmoitusL.Size = new System.Drawing.Size(224, 20);
+            this.IlmoitusL.TabIndex = 20;
+            this.IlmoitusL.Text = "Paina formia pitääksesi pisteet";
+            this.IlmoitusL.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 317);
+            this.Controls.Add(this.IlmoitusL);
+            this.Controls.Add(this.checkBox_botStart);
+            this.Controls.Add(this.checkBox_bot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnVaihto);
+            this.Controls.Add(this.btnNollaa);
+            this.Controls.Add(this.VuoroL);
+            this.Controls.Add(this.txtOpisteet);
+            this.Controls.Add(this.txtXpisteet);
+            this.Controls.Add(this.labelO);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.b2_2);
+            this.Controls.Add(this.b1_2);
+            this.Controls.Add(this.b0_2);
+            this.Controls.Add(this.b2_1);
+            this.Controls.Add(this.b2_0);
+            this.Controls.Add(this.b1_1);
+            this.Controls.Add(this.b1_0);
+            this.Controls.Add(this.b0_1);
+            this.Controls.Add(this.b0_0);
+            this.Name = "Form1";
+            this.Text = "RistiNolla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b0_0;
+        private System.Windows.Forms.Button b0_1;
+        private System.Windows.Forms.Button b1_1;
+        private System.Windows.Forms.Button b1_0;
+        private System.Windows.Forms.Button b2_1;
+        private System.Windows.Forms.Button b2_0;
+        private System.Windows.Forms.Button b2_2;
+        private System.Windows.Forms.Button b1_2;
+        private System.Windows.Forms.Button b0_2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelO;
+        private System.Windows.Forms.Label VuoroL;
+        private System.Windows.Forms.Button btnNollaa;
+        private System.Windows.Forms.Button btnVaihto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox_bot;
+        private System.Windows.Forms.CheckBox checkBox_botStart;
+        private System.Windows.Forms.TextBox txtXpisteet;
+        private System.Windows.Forms.TextBox txtOpisteet;
+        private System.Windows.Forms.Label IlmoitusL;
+    }
+}
+
---
+++ Ristinolla V1.0/Ristinolla V1.0/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ristinolla_V1._0
+{
+    public partial class Form1 : Form
+    {
+
+        //mahdollisia parannuksia olisi lisätä botin älykkyyttä, jotta se voisi voittaa pelaajan.
+        //eri vaikeustasoja ja kentän kokoja
+
+
+        //boolit
+        bool nextmarkX = true;
+        bool botTurn = false;
+        bool PlayerAsO = false;
+        bool botplaying = false;
+
+        bool winner = false;
+        //pisteet
+        int xPisteet = 0;
+        int oPisteet = 0;
+        //random
+        Random random = new Random();
+
+        int randomvalinta = 0;
+        int clickcount = 0;
+        //viimeksi 
+        Button lastClickedButton = null;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseClick += new MouseEventHandler(nollaus);
+            UpdateButtonHandlers();
+        }
+        private void UpdateButtonHandlers()
+        {
+            foreach (Control control in this.Controls) // hakee fromsista kaikki lisätyt objektit
+            {
+                if (control is Button button && button.Tag == null)          // jos control on nappi 
+                {
+                    button.Click += napit;             // jos nappia painetaan suorittaa napit methodin
+                }
+
+            }
+        }
+        private void napit(object sender, EventArgs e)
+        {
+            if (winner == false)                    // jos voittajaa ei ole löytynyt
+            {
+                if (sender is Button button)        // tarkistaa jos sender oli nappi. Välttää ylimääräisen foreach loopin
+                {
+                    if (string.IsNullOrEmpty(button.Text))  // jos napissa ei ole merkkiä.
+                    {
+                        lastClickedButton = button; //tallentaa viimeksi painetun napin
+                        clickcount++;               //lisää painettujen nappien määrää
+
+                        /*
+                        jos botti on päällä, vaihtaa vuoron ja merkin botin vuoroksi
+                        eli jos pelaaja on O, vaihtaa vuoron  X:ksi
+                         * jos pelaaja on X, vaihtaa vuoron O:ksi
+                         * 
+                         */
+                        if (botplaying == true)
+                        {
+                            if (PlayerAsO == true)      //vuoro järjestelmä
+                            {
+                                button.Text += "O";
+                                VuoroL.Text = "Vuoro: X";//vaihtaa vuoron tekstin
+                                nextmarkX = !nextmarkX; //vaihtaa vuoron
+                                botTurn = !botTurn;
+                                Voitto();
+                                checkBox_bot_CheckedChanged(sender, e);
+                            }
+                            else
+                            {
+                                button.Text = "X";
+                                VuoroL.Text = "Vuoro: O";//vaihtaa vuoron tekstin
+                                nextmarkX = !nextmarkX;  //vaihtaa vuoron
+                                botTurn = !botTurn;
+                                Voitto();
+                                checkBox_bot_CheckedChanged(sender, e);
+
+                            }
+                        }
+                        else
+                        {   // Tarkistetaan, onko vuorossa merkki "O" välttääkseen saman kuvion tulolstumista molemmille pelaajille.
+                            if (nextmarkX == false)
+                            {
+                                button.Text += "O";
+                                VuoroL.Text = "Vuoro: X";
+                                nextmarkX = !nextmarkX;
+                                botTurn = !botTurn;
+                                Voitto();
+                                checkBox_bot_CheckedChanged(sender, e);
+                            }
+                            else
+                            {
+                                button.Text = "X";
+                                VuoroL.Text = "Vuoro: O";
+                                nextmarkX = !nextmarkX;
+                                botTurn = !botTurn;
+                                Voitto();
+                                checkBox_bot_CheckedChanged(sender, e);
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void Voitto()
+        {
+            if (winner)
+            {
+                return;
+            }
+            //hakee kaikki napit
+            Button[][] voittokombinaatiot = new Button[][]
+            {
+                new Button[] { b0_0, b0_1, b0_2 },
+                new Button[] { b1_0, b1_1, b1_2 },
+                new Button[] { b2_0, b2_1, b2_2 },
+
+                new Button[] { b0_0, b1_0, b2_0 },
+                new Button[] { b0_1, b1_1, b2_1 },
+                new Button[] { b0_2, b1_2, b2_2 },
+
+                new Button[] { b0_0, b1_1, b2_2 },
+                new Button[] { b0_2, b1_1, b2_0 }
+            };
+            foreach (Button[] btn in voittokombinaatiot)
+            {
+                //tarkistaa jos napit ovat samat ja eivät ole tyhjiä
+                if (!string.IsNullOrEmpty(btn[0].Text) && btn[0].Text == btn[1].Text && btn[1].Text == btn[2].Text) 
+                {
+                        string voittaja = btn[0].Text;
+                    if (voittaja == "X")
+                    {
+                        xPisteet++;
+                        txtXpisteet.Text = xPisteet.ToString();
+                        
+                    }
+                    else 
+                    {
+                        oPisteet++;
+                        txtOpisteet.Text = oPisteet.ToString();
+                        
+                    }
+                    foreach (Button button in btn)
+                    {
+                        button.BackColor = Color.Blue;
+                    }
+
+                    winner = true;
+                    VuoroL.Text = "Voittaja: " + voittaja;
+                    IlmoitusL.Visible = true;
+                } 
+            }
+        }
+        private void nollaus(object sender, EventArgs e)
+        {
+
+            //jos peli on pelattu, resettaa kaikki napit. 
+            if (winner == true)
+            {
+                // hakee fromsista kaikki lisätyt objektit
+                foreach (Control control in this.Controls)
+                {
+                    if (control is Button button && button.Tag == null) //jos objeckti on buttoni ja tag on tyhjä, tyhjentää (kaikilla muilla napeilla on oma tag)
+                    {
+                        button.Text = string.Empty;
+                        button.BackColor = Color.White;
+                    }
+                }
+                //Nollaa arvot aluku asemiin
+                if (PlayerAsO == false)
+                {
+                    nextmarkX = true;
+                    VuoroL.Text = "Vuoro: X";
+                }
+                else
+                {
+                    nextmarkX = false;
+                    VuoroL.Text = "Vuoro: O";
+                }
+                //nollaa arvot
+                clickcount = 0;
+                winner = false;
+                botTurn = false;
+                IlmoitusL.Visible = false;
+                checkBox_botStart_CheckedChanged(sender, e);
+            }
+        }
+        private void btnNollaa_Click(object sender, EventArgs e)
+        {
+            //nollaa arvot
+            winner = true;
+            xPisteet = 0;
+            oPisteet = 0;
+            txtOpisteet.Text = oPisteet.ToString();
+            txtXpisteet.Text = xPisteet.ToString();
+            nollaus(sender, e);
+        }
+
+        private async void checkBox_bot_CheckedChanged(object sender, EventArgs e)
+        {
+            //hakee kaikki napit
+            Button[] buttons = { b0_0, b0_1, b0_2, b1_0, b1_1, b1_2, b2_0, b2_1, b2_2 };
+            if (checkBox_bot.Checked)
+            {
+                botplaying = true;
+            }
+            else if (!checkBox_bot.Checked)
+            {
+                botplaying = false;
+            }
+
+            //botin toiminta
+            while (checkBox_bot.Checked && winner == false && botTurn == true)
+            {
+                await Task.Delay(200); // odottaa 200ms ennen kuin tekee siirron
+                randomvalinta = random.Next(0, buttons.Length); // arpoo satunnaisen luvun buttonien määrän mukaan
+                                                                // jos satunnainen luku on sama kuin käyttäjän painama tai kenttä on jo täynnä, ohjelma skippaa lopun loopin koodin ja menee alkuun.
+                if (buttons[randomvalinta] == lastClickedButton || !string.IsNullOrEmpty(buttons[randomvalinta].Text))
+                {
+                    continue;
+                }
+
+                if (PlayerAsO == false)
+                { // jos pelaaja on X
+                    buttons[randomvalinta].Text += "O"; //luvun mukaan merkitsee nappiin O merkin
+                    VuoroL.Text = "Vuoro: X";           //  vuoron vaihto
+                    nextmarkX = !nextmarkX;             // 
+                    clickcount++;                       //  Painettujen nappien määrä +1
+                    botTurn = false;                    //  kierros pois
+                    Voitto();                           // tarkistaa voiton                     
+                    break;
+                }
+                else
+                { // jos pelaaja on O
+                    buttons[randomvalinta].Text += "X"; //sama kuin yllä mutta X merkillä
+                    VuoroL.Text = "Vuoro: O";
+                    nextmarkX = !nextmarkX;
+                    botTurn = !botTurn;
+                    clickcount++;
+                    botTurn = false;
+                    Voitto();
+                    break;
+                }
+
+            }
+        }
+
+        // Vaihtaa pelaajan kirjaimen
+        private void btnVaihto_Click(object sender, EventArgs e)
+        {   //vaihtaa pelaajan kirjaimen
+            if (clickcount == 0)
+            {
+                PlayerAsO = !PlayerAsO;
+                nextmarkX = !nextmarkX;
+                if (nextmarkX == true)
+                {   //vaihtaa vuoron tekstit
+                    btnVaihto.Text = "X";
+                    VuoroL.Text = "Vuoro: X";
+                }
+                else
+                {   //vaihtaa vuoron tekstit
+                    btnVaihto.Text = "O";
+                    VuoroL.Text = "Vuoro: O";
+                }
+                checkBox_botStart_CheckedChanged(sender, e);
+            }
+        }
+
+        private void checkBox_botStart_CheckedChanged(object sender, EventArgs e)
+        {   //botti aloittaa
+            if (clickcount == 0 && checkBox_botStart.Checked)
+            {
+                botTurn = true;
+                checkBox_bot_CheckedChanged(sender, e);
+            }
+        }
+    }
+}
